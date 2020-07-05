ede1f1181cbd200408818a863e0805d48ff1260f
Braden Coates <xellus@pop-os.localdomain>
2020-07-05 11:44:29 -0400

    added meta tags to index

--- server.js
+++ server.js
@@ -15,11 +15,13 @@ const errorRoute = require("./routes/404.js");
 const headerRoute = require("./routes/header.js");
 const indexRoute = require("./routes/index.js");
 const resumeRoute = require("./routes/resume.js");
-const contactRoute = require("./routes/contact.js")
+const contactRoute = require("./routes/contact.js");
+const projectsRoute = require("./routes/projects.js");
 
 // Making routes
 app.use("/header", headerRoute);
 app.use("/", indexRoute);
+app.use("/projects", projectsRoute);
 app.use("/resume", resumeRoute);
 app.use("/contact", contactRoute);
 app.use("*", errorRoute);
